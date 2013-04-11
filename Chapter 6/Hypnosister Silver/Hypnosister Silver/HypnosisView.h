d3b439aea552b47580962c02245174c185f79a5a
Erin Parker <erinchanparker@gmail.com>
2013-04-11 14:40:21 -0700

    Updated folder name to Silver

---
+++ Chapter 6/Hypnosister Silver/Hypnosister Silver/HypnosisView.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisView.h
+//  Hypnosister Silver
+//
+//  Created by Erin Parker on 4/10/13.
+//  Copyright (c) 2013 com.bignerdranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisView : UIView
+
+@property (nonatomic, strong) UIColor *circleColor;
+
+@end
