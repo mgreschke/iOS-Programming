c69eba041c99c19fe4cc64d50d2ed27897e4602b
Erin Parker <erinchanparker@gmail.com>
2013-04-13 16:18:12 -0700

    Committing hypnosisviewcontroller.m

--- Chapter 7/HypnoTime Silver/HypnosisViewController.m
+++ Chapter 7/HypnoTime Silver/HypnosisViewController.m
@@ -8,6 +8,7 @@
 
 #import "HypnosisViewController.h"
 #import "HypnosisView.h"
+#import "HypnosisView.m"
 
 @interface HypnosisViewController ()
 
@@ -32,14 +33,51 @@
         
         UIImage *i = [UIImage imageNamed:@"Hypno.png"];
         [tbi setImage:i];
+    
     }
     return self;
 }
+
+- (void)createControls
+{
+    UISegmentedControl *segmentedControl = [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"Red", @"Green", @"Blue", nil]];
+    
+    segmentedControl.segmentedControlStyle = UISegmentedControlStylePlain;
+    
+    [segmentedControl addTarget:self action:@selector(createControls:) forControlEvents:UIControlEventValueChanged];
+    
+    [self.view addSubview:segmentedControl];
+}
+
+- (IBAction)createControls:(id)sender
+{
+    switch (((UISegmentedControl *)sender).selectedSegmentIndex)
+    {
+        case 0:
+        {
+            NSLog(@"Red Color");
+            [circleColor redColor];
+            break;
+        }
+        case 1:
+        {
+            NSLog(@"Green Color");
+            break;
+        }
+        default:
+        {
+            NSLog(@"Blue Color");
+            break;
+        }
+    }
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     NSLog(@"HypnosisViewController loaded its view.");
+    [self createControls];
 }
 
 - (void)didReceiveMemoryWarning
