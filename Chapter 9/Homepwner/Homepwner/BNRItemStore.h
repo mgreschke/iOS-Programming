51b376f23b5e8f9646e2a9d73dae5c63395c0df6
Erin Parker <erinchanparker@gmail.com>
2013-04-15 15:08:08 -0700

    Chapter 9 Homepwner basic app done

---
+++ Chapter 9/Homepwner/Homepwner/BNRItemStore.h
@@ -0,0 +1,22 @@
+//
+//  BNRItemStore.h
+//  Homepwner
+//
+//  Created by Erin Parker on 4/15/13.
+//  Copyright (c) 2013 com.bignerdranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@class BNRItem;
+
+@interface BNRItemStore : NSObject
+{
+    NSMutableArray *allItems;
+}
+
++ (BNRItemStore *)sharedStore;
+
+- (NSArray *)allItems;
+- (BNRItem *)createItem;
+
+@end
