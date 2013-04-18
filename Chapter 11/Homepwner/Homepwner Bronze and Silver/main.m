a3991dcf24c3f11dc0030fe1f220d9ea9881e2d6
Erin Parker <erinchanparker@gmail.com>
2013-04-18 00:23:42 -0700

    Completed Chapter 11 Bronze and Silver Exercises

---
+++ Chapter 11/Homepwner/Homepwner Bronze and Silver/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Erin Parker on 4/15/13.
+//  Copyright (c) 2013 com.bignerdranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HomepwnerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HomepwnerAppDelegate class]));
+    }
+}
