17411b1d86b2c9b29a52e54b2cd4a1578ead9f31
Erin Parker <erinchanparker@gmail.com>
2013-04-19 12:57:56 -0700

    Finished Ch 24 Storyboards

---
+++ Chapter 24/Storytime/Storytime/ModalViewController.h
@@ -0,0 +1,15 @@
+//
+//  ModalViewController.h
+//  Storytime
+//
+//  Created by Erin Parker on 4/19/13.
+//  Copyright (c) 2013 Never Bend Easy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+
+- (IBAction)dismiss: (id)sender;
+
+@end
---
+++ Chapter 24/Storytime/Storytime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Storytime
+//
+//  Created by Erin Parker on 4/19/13.
+//  Copyright (c) 2013 Never Bend Easy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NBEAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NBEAppDelegate class]));
+    }
+}
