ea52c66633a8df24033e0379e67aca87a053d4a4
Erin Parker <erinchanparker@gmail.com>
2013-08-26 17:14:29 -0700

    Added Chapter 13 UIPopover Controller Modal View Controller

---
+++ Chapter 13/Homepwner/BNRItem.h
@@ -0,0 +1,32 @@
+//
+//  BNRItem.h
+//  RandomPossessions
+//
+//  Created by Erin Parker on 4/6/13.
+//  Copyright (c) 2013 com.bignerdranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+
++ (id)randomItem;
+
+- (id)initWithItemName: (NSString *)name
+        valueInDollars: (int)value
+          serialNumber: (NSString *)sNumber;
+
+- (id)initWithItemName: (NSString *)name
+          serialNumber: (NSString *)sNumber;
+
+@property (nonatomic, strong) BNRItem *containedItem;
+@property (nonatomic, weak) BNRItem *container;
+
+@property (nonatomic, copy) NSString *itemName;
+@property (nonatomic, copy) NSString *serialNumber;
+@property (nonatomic) int valueInDollars;
+@property (nonatomic, readonly, strong) NSDate *dateCreated;
+
+@property (nonatomic, copy) NSString *imageKey;
+
+@end
